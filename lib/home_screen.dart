a6f9ad7a250ab8cbf995f7b7d3f0f7faa112abfe
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-08-16 18:57:26 +0600

    added more sounds

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,42 +1,75 @@
-import 'dart:developer';
-
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  Widget soundButton({
+    required String text,
+    required String sound,
+    required String image,
+  })
+  {
+    return Column(
+      children: [
+        Text(text, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        SizedBox(height: 5),
+        InkWell(
+          onTap: () {
+            final player = AudioPlayer();
+            player.play(AssetSource('${sound}_sound.wav'));
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white70,
+              borderRadius: BorderRadius.circular(
+                15,
+              ), // set any radius you like
+            ),
+            padding: EdgeInsets.all(12),
+            child: Image(image: AssetImage("images/$image.png"), width: 130),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("App Bar"),
-      ),
+      appBar: AppBar(title: Text("App Bar")),
       backgroundColor: Colors.white60,
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Row(
-                children: [
-                  ElevatedButton(onPressed: (){
-                    log("clicked");
-                  },
-                    style: ButtonStyle(
-                      padding: WidgetStateProperty.all(EdgeInsets.zero),
-                    ),
-                    child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15), // set any radius you like
-                    ),
-                    padding: EdgeInsets.all(12),
-                    child: Image(
-                      image: AssetImage("images/wave.png"),
-                      width: 130,
-                    ),
-                  ),)
-                ],
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    soundButton(text: "Wave", sound: "wave", image: "wave"),
+
+                    soundButton(text: "Pistol", sound: "pistol", image: "pistol"),
+
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    soundButton(text: "Bird", sound: "bird", image: "bird"),
+
+                    soundButton(text: "Drum", sound: "drum", image: "drum"),
+
+                  ],
+                ),
               ),
             ],
           ),
